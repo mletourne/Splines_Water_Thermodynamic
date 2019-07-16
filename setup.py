439691c68fb57e2df3847743c149c9d579ef7e3a
mletourne <39578916+mletourne@users.noreply.github.com>
2019-07-16 14:28:54 +1200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='ThSpl',
+    version='1.0.0',
+    description='Spline-based water thermodynamics properties formulation',
+    author='Martin Letourneur',
+    author_email='martin.letourn@gmail.com',
+    url='https://github.com/mletourne/Splines_Water_Thermodynamic',
+    license='MIT',
+    py_modules=['th_spl'],
+	)
